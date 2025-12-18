ae058f38e65432af3360c01ac0cc4326965f14c9
FDR-3 <fdr3@m4a.io>
2025-12-18 09:31:44 -0500

    renamed the mocked account to be clearer that it's not a pda, and a comment to explain why it's a signer.

--- programs/pyth-mock/src/lib.rs
+++ programs/pyth-mock/src/lib.rs
@@ -10,7 +10,7 @@ pub mod pyth_mock
 
     pub fn set_mocked_pyth_price_update_account(ctx: Context<SetMockPythPriceUpdateData>, data: Vec<u8>) -> Result<()>
     {
-        let account_data = ctx.accounts.mocked_pyth_price_update_pda.to_account_info().data;
+        let account_data = ctx.accounts.mocked_pyth_price_update_account.to_account_info().data;
         let borrow_data = &mut *account_data.borrow_mut();
 
         Ok((&mut borrow_data[0..]).write_all(&data[..])?)
@@ -20,6 +20,7 @@ pub mod pyth_mock
 #[derive(Accounts)]
 pub struct SetMockPythPriceUpdateData<'info> 
 {
+    //Using a generic signer account instead of an anchor struct so that anchor doesn't force account discriminator to be something specific
     #[account(mut)]
-    mocked_pyth_price_update_pda: Signer<'info>,
+    mocked_pyth_price_update_account: Signer<'info>,
 }
